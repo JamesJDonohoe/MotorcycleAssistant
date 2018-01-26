3ff8c09b204378eb1afe835c165913f3c98cad11
JamesJDonohoe <c13452398@mydit.ie>
2018-01-26 17:50:04 +0000

    Code Rejig, minor function tweaks

--- app/src/main/java/com/example/james/motorcycleassistant/TrackingActivity.java
+++ app/src/main/java/com/example/james/motorcycleassistant/TrackingActivity.java
@@ -1,14 +1,24 @@
+/**
+ * Created by James Donohoe C13452398
+ * This is the main activity. All of the main functionality will be in this class.
+ * The users location will be tracked and all of the sensor data will be read from here.
+ *
+ * https://developers.google.com/maps/documentation/android-api/map
+ * **/
+
+
 package com.example.james.motorcycleassistant;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -30,10 +40,11 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         LocationListener{
 
     private GoogleMap mMap;
+    Marker LocationMarker;
     GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
-    Marker mCurrLocationMarker;
-    LocationRequest mLocationRequest;
+    
+    Location getLastLocation;
+    LocationRequest userLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,7 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Gets map fragment and allows map to display location
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -79,17 +90,18 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         mGoogleApiClient.connect();
     }
 
+    //Uses GPS sensor to read location of device
     @Override
     public void onConnected(Bundle bundle) {
-
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //Used to get an accurate location of the device
+        userLocationRequest = new LocationRequest();
+        userLocationRequest.setInterval(1000);
+        userLocationRequest.setFastestInterval(1000);
+        userLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         if (ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, userLocationRequest, this);
         }
 
     }
@@ -102,18 +114,19 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onLocationChanged(Location location) {
 
-        mLastLocation = location;
-        if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
+        //This gets the Current/Last Location an stores it in a variable called location
+        getLastLocation = location;
+        if (LocationMarker != null) {
+            LocationMarker.remove();
         }
 
-        //Place current location marker
+        //Uses LatLng to get the location and display a marker at the location
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng);
         markerOptions.position(latLng);
-        markerOptions.title("Start Location");
+        markerOptions.title("Starting Location");
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-        mCurrLocationMarker = mMap.addMarker(markerOptions);
+        LocationMarker = mMap.addMarker(markerOptions);
 
         //move camera to current location
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,10);
@@ -127,10 +140,11 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
+    //Handles users permission request. Reads into the manifest to ask for location
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     public boolean checkLocationPermission(){
         if (ContextCompat.checkSelfPermission(this,
@@ -158,12 +172,12 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if ((grantResults.length > 0)
+                        && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
 
                     // permission was granted. Do the
                     // contacts-related task you need to do.
@@ -179,8 +193,12 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
 
                 } else {
 
-                    // Permission denied, Disable the functionality that depends on this permission.
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                    // If permission is declined location will not be displayed.
+                    Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_LONG).show();
+                    //User will be brought back to the main menu
+                    Intent intent = new Intent(TrackingActivity.this, Screen1.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
 
